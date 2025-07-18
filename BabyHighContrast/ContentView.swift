e111257890bf1def8a68936811bdc8380eb34677
Daniel Callisaya <danielj.callisayah@gmail.com>
2025-07-18 11:48:21 -0400

    Enhance ContentView by adding selection state to TabView, allowing for circular navigation between animation views. Updated tab view style and added logic to handle selection changes.

--- BabyHighContrast/ContentView.swift
+++ BabyHighContrast/ContentView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 1
     var body: some View {
-        TabView {
-            AnimationView1()
-            AnimationView2()
-            AnimationView3()
-            AnimationView4()
+        TabView(selection: $selection) {
+            AnimationView4().tag(0)
+            AnimationView1().tag(1)
+            AnimationView2().tag(2)
+            AnimationView3().tag(3)
+            AnimationView4().tag(4)
+            AnimationView1().tag(5)
         }
-        .tabViewStyle(PageTabViewStyle())
+        .tabViewStyle(.page(indexDisplayMode: .never))
         .ignoresSafeArea()
+        .onChange(of: selection) { oldValue, newValue in
+            if newValue == 0 {
+                selection = 4
+            } else if newValue == 5 {
+                selection = 1
+            }
+        }
     }
 }
 
